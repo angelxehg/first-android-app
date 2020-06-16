c4fba9bbd1e7f23ead50b1f549de5e6583ac96fb
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-15 20:55:47 -0500

    Simple theme selector

--- app/src/main/java/com/angelxehg/first/android/app/MainActivity.java
+++ app/src/main/java/com/angelxehg/first/android/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.angelxehg.first.android.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
         String value = edt1.getText().toString();
         i.putExtra(data, value);
         startActivity(i);
+    }
+
+    public void setDarkTheme(View view) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    public void setLightTheme(View view) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    public void setAutoTheme(View view) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
     }
 
 }
