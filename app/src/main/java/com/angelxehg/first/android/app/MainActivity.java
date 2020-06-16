c64ee99ae867213fb45aeb63f8cc4d1ec77f8971
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-15 19:33:50 -0500

    Create DataActivity

--- app/src/main/java/com/angelxehg/first/android/app/MainActivity.java
+++ app/src/main/java/com/angelxehg/first/android/app/MainActivity.java
@@ -2,12 +2,16 @@ package com.angelxehg.first.android.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String data = "com.angelxehg.first.android.app.DATA";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +19,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showData(View view){
-        Log.i("info", "Pushed");
+        Intent i = new Intent(this, DataActivity.class);
+        EditText edt1 = (EditText) findViewById(R.id.editText);
+        String value = edt1.getText().toString();
+        i.putExtra(data, value);
+        startActivity(i);
     }
 
 }
