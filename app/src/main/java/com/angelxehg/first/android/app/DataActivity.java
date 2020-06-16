827c994c314cd0d012d4782c0f1711e1206277ed
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-15 19:39:49 -0500

    Show data on DataActivity

--- app/src/main/java/com/angelxehg/first/android/app/DataActivity.java
+++ app/src/main/java/com/angelxehg/first/android/app/DataActivity.java
@@ -2,7 +2,9 @@ package com.angelxehg.first.android.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DataActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class DataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+        Intent i = getIntent();
+        String value = i.getStringExtra(MainActivity.data);
+        TextView tv1 = findViewById(R.id.textView);
+        tv1.setText(value);
     }
 }
