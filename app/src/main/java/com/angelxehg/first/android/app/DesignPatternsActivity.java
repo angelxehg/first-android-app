4d3e5078aefda8d81a5d078036aebca854773696
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-16 20:26:49 -0500

    Play with linear layout

--- app/src/main/java/com/angelxehg/first/android/app/DesignPatternsActivity.java
+++ app/src/main/java/com/angelxehg/first/android/app/DesignPatternsActivity.java
@@ -2,7 +2,9 @@ package com.angelxehg.first.android.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class DesignPatternsActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class DesignPatternsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_design_patterns);
+        LinearLayout ly = new LinearLayout(getApplicationContext());
+        ly.setOrientation(LinearLayout.VERTICAL);
+        ly.setBackgroundColor(Color.GRAY);
+        setContentView(ly);
     }
 }
