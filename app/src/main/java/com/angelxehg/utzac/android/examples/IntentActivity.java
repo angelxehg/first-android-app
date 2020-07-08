a579e7e0b9e84dad0b17f7531b20b6d89f3be4a1
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-07-07 20:08:05 -0500

    Create IntentActivity

---
+++ app/src/main/java/com/angelxehg/utzac/android/examples/IntentActivity.java
@@ -0,0 +1,19 @@
+package com.angelxehg.utzac.android.examples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class IntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+
+    public void openBrowser(View view) {
+
+    }
+}
