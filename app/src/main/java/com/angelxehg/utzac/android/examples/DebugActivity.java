6d14b6cf5b7b9680ed9177eb10a7b292a978c459
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-07-13 20:17:56 -0500

    Create debug activity

---
+++ app/src/main/java/com/angelxehg/utzac/android/examples/DebugActivity.java
@@ -0,0 +1,16 @@
+package com.angelxehg.utzac.android.examples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.angelxehg.utzac.android.examples.android.app.R;
+
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+    }
+}
