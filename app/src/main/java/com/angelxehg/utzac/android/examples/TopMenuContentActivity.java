de856c07632ca62b79b69cb4b7cfb335682ccb22
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-08-04 20:02:12 -0500

    Create new activity

---
+++ app/src/main/java/com/angelxehg/utzac/android/examples/TopMenuContentActivity.java
@@ -0,0 +1,17 @@
+package com.angelxehg.utzac.android.examples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.angelxehg.utzac.android.examples.android.app.R;
+
+import android.os.Bundle;
+
+public class TopMenuContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_menu_content);
+    }
+}
